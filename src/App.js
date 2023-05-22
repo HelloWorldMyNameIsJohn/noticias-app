f790194d08ea590c257950ada95d8fc04c0afa05
John <Arthur_John_Paul@outlook.com>
2023-05-21 20:32:11 -0600

    se quito el homepage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-            <Route exact path='/noticias-app/' element={<Home />} >
+            <Route exact path='/' element={<Home />} >
             </Route>
         </Routes>
       </BrowserRouter>
